a843f1c3b5f59d9c1faaf9ab13a19680c7ec8793
DallasIX <raghavpillai101@gmail.com>
2021-06-26 14:39:29 -0500

    Push1

---
+++ creditValidation.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class creditValidation {
+    long creditNumber;
+    scanner Scanner = new Scanner(System.in);
+    
+    public void getInput(){
+        System.out.println("Enter a credit card number as a long integer: ");
+        creditNumber = scanner.nextInt();
+    }
+
+    /** Return true if the card number is valid */
+    public static boolean isValid(long number){
+
+    }
+    /** Get the result from Step 2 */
+    public static int sumOfDoubleEvenPlace(long number){
+
+    }
+    /** Return this number if it is a single digit, otherwise,
+    * return the sum of the two digits */
+    public static int getDigit(int number){
+
+    }
+    /** Return sum of odd-place digits in number */
+    public static int sumOfOddPlace(long number){
+
+    }
+    /** Return true if the digit d is a prefix for number */
+    public static boolean prefixMatched(long number, int d){
+
+    }
+    /** Return the number of digits in d */
+    public static int getSize(long d){
+
+    }
+    /** Return the first k number of digits from number. If the
+    * number of digits in number is less than k, return number. */
+    public static long getPrefix(long number, int k)
+    
+    public static void main(String[] args){
+
+    }
+}
